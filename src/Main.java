2d7463c562d928627c0f2ef2da22e3708a19e95a
AndreyPascoa <andrey.pascoa@gmail.com>
2023-10-10 17:19:08 -0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         
+        ArrayList<Pessoa> listaPessoa = new ArrayList<>();
+
+        int codigo, opc = 0;
+        String nome, cpf, curso, funcao, ra, setor, urlCurriculoLattes;
+        double salario;
+
+        while(opc != 7){
+            opc = Integer.parseInt(JOptionPane.showInputDialog("1-Inserir aluno\n2-Inserir professor\n3-Inserir Atendent\n4-Inserir aluno\n5-Adicionar disciplina ao professor\n6-Mostrar pessoas\n7-Sair"));
+            switch(opc){
+                case 1:
+                                        
+                break;
+            }
+        }
     }
 }
